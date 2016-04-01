bb16889f56b57b259c0f58d88b099fa2f4d5193a
John Ritsema <john_ritsema@yahoo.com>
2016-04-01 11:42:47 -0400

    initial

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_id" {
+}
+
+variable "vpc_cidr_block" {
+}
+
+variable "cache_name" {
+}
+
+variable "engine_version" {
+  default = "2.8.22"
+}
+
+variable "instance_type" {
+  default = "cache.t2.micro"
+}
+
+variable "maintenance_window" {
+  # SUN 01:00AM-02:00AM ET
+  default = "sun:05:00-sun:06:00"
+}
+
+variable "private_subnet_ids" {
+}
+
+variable "alarm_actions" {
+}
